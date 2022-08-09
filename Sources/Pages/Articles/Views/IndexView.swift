9849b66014f784d2e0ce499bae01c8ab55448b38
Shahrukh Alam <alam.shahrukh786@gmail.com>
2022-08-09 14:08:09 +0530

    corrects css paths

--- Sources/Pages/Articles/Views/IndexView.swift
+++ Sources/Pages/Articles/Views/IndexView.swift
@@ -20,11 +20,11 @@ public let indexView = Document {
             Title("Server Side Swift Article Tutorial, max 50-60 character, go for long descriptive title")
             Meta([.characterSet(.utf8),
                   .name(.author, content: "Shahrukh Alam")])
-            CSSLink(path: "../CSS/kickoff.css")
-            CSSLink(path: "../CSS/nav.css")
-            CSSLink(path: "../CSS/body.css")
-            CSSLink(path: "../CSS/article.css")
-            CSSLink(path: "../CSS/markdown.css")
+            CSSLink(path: "CSS/kickoff.css")
+            CSSLink(path: "CSS/nav.css")
+            CSSLink(path: "CSS/body.css")
+            CSSLink(path: "CSS/article.css")
+            CSSLink(path: "CSS/markdown.css")
         }
         
         Body {
