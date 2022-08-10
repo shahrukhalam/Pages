00b47ad152b37319425872391957241b044690cc
Shahrukh Alam <shahrukh@swapcard.com>
2022-08-10 13:27:03 +0530

    test0 viewport

--- Sources/Pages/Articles/Views/IndexView.swift
+++ Sources/Pages/Articles/Views/IndexView.swift
@@ -13,7 +13,7 @@ public func indexView(article: Article) -> some View {
         HTML {
             Head {
                 Title("Server Side Swift Article Tutorial, max 50-60 character, go for long descriptive title")
-                Meta([.characterSet(.utf8), .name(.author, content: "Shahrukh Alam")])
+                Meta([.viewport(width: .deviceWidth, scale: .full)])
                 CSSLink(path: "CSS/kickoff.css")
                 CSSLink(path: "CSS/nav.css")
                 CSSLink(path: "CSS/body.css")
