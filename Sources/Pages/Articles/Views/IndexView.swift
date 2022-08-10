7869649e19fe8453176a6a1f0bd5f831d1e1d549
Shahrukh Alam <shahrukh@swapcard.com>
2022-08-10 13:57:09 +0530

    corrects meta attribute one by one

--- Sources/Pages/Articles/Views/IndexView.swift
+++ Sources/Pages/Articles/Views/IndexView.swift
@@ -13,14 +13,10 @@ public func indexView(article: Article) -> some View {
         HTML {
             Head {
                 Title("Server Side Swift Article Tutorial, max 50-60 character, go for long descriptive title")
-                Meta([.viewport(width: .deviceWidth, scale: .full)])
-                CSSLink(path: "CSS/kickoff.css")
-                CSSLink(path: "CSS/nav.css")
-                CSSLink(path: "CSS/body.css")
-                CSSLink(path: "CSS/article.css")
-                CSSLink(path: "CSS/markdown.css", type: .wide)
-                CSSLink(path: "CSS/markdown-small.css", type: .small)
-                CSSLink(path: "CSS/source.css")
+                Meta(.characterSet(.utf8))
+                Meta(.name(.author, content: "Shahrukh Alam"))
+                Meta(.viewport(width: .deviceWidth, scale: .full))
+                commonCSS
             }
             
             Body {
@@ -31,6 +27,18 @@ public func indexView(article: Article) -> some View {
             }
         }
     }
+}
+
+private var commonCSS: some HTMLContentView {
+    AnyView([
+        CSSLink(path: "CSS/kickoff.css"),
+        CSSLink(path: "CSS/nav.css"),
+        CSSLink(path: "CSS/body.css"),
+        CSSLink(path: "CSS/article.css"),
+        CSSLink(path: "CSS/markdown.css", type: .wide),
+        CSSLink(path: "CSS/markdown-small.css", type: .small),
+        CSSLink(path: "CSS/source.css")
+    ])
 }
 
 private let highlightScriptURL = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.2/highlight.min.js"
