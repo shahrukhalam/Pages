d382565013f7c16f7351bcc17b40b9c57bafbb33
Shahrukh Alam <alam.shahrukh786@gmail.com>
2022-10-16 16:04:15 +0530

    uses remote HTMLDSL

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "Pages", targets: ["Pages"])
     ],
     dependencies: [
-        .package(path: "../HTMLDSL")
+        .package(url: "https://github.com/shahrukhalam/HTMLDSL", branch: "main")
     ],
     targets: [
         .target(name: "Pages", dependencies: ["HTMLDSL"]),
